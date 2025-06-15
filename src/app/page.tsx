3a26ae21ceaa1f9290c7c1aeafef0e24fec711d7
4snt <murilo.escobedo@protonmail.com>
2025-06-15 19:30:39 -0300

    fix(tsconfig.json): corrijindo tsconfig para build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Container from "@/components/Container";
-import HomeBanner from "@/components/HomeBanner";
 import ProductGrid from "@/components/products/ProductGrid";
 import { SearchParams } from "@/types/SearchParams";
+import Container from "../components/Container";
+import HomeBanner from "../components/HomeBanner";
 
-export default function Home({
-  searchParams = {},
-}: {
+type HomeProps = {
   searchParams?: SearchParams;
-}) {
+};
+
+export default function Home({ searchParams = {} }: HomeProps) {
   return (
     <div className="p-8">
       <Container>
