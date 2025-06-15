3d11b38293ca3feb941d0ec1af2802cd68db6e2e
4snt <murilo.escobedo@protonmail.com>
2025-06-15 20:10:31 -0300

    (fix)?: vou ficar lele

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
 import ProductGrid from "@/components/products/ProductGrid";
-import { SearchParams } from "@/types/SearchParams";
 
-interface HomeProps {
-  searchParams: SearchParams;
+interface SearchParams {
+  [key: string]: string | string[] | undefined;
 }
 
-export default async function Home({ searchParams }: HomeProps) {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
   return (
     <div className="p-8">
       <Container>
