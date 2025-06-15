4df7f0f9cb4760a34f27c5719f76311fdd9f51b9
4snt <murilo.escobedo@protonmail.com>
2025-06-15 19:24:43 -0300

    fix(types): remover tipagem incorreta com Promise em page props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
 import ProductGrid from "@/components/products/ProductGrid";
 import { SearchParams } from "@/types/SearchParams";
-import Container from "../components/Container";
-import HomeBanner from "../components/HomeBanner";
 
-type HomeProps = {
+export default function Home({
+  searchParams = {},
+}: {
   searchParams?: SearchParams;
-};
-
-export default function Home({ searchParams = {} }: HomeProps) {
+}) {
   return (
     <div className="p-8">
       <Container>
