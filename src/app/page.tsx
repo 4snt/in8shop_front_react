c2a5254e56fd09e3bf20f0807ddfe708cd87aca1
4snt <murilo.escobedo@protonmail.com>
2025-06-15 19:42:20 -0300

    fix(home) async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
 import ProductGrid from "@/components/products/ProductGrid";
 import { SearchParams } from "@/types/SearchParams";
-import Container from "../components/Container";
-import HomeBanner from "../components/HomeBanner";
 
-type HomeProps = {
-  searchParams?: SearchParams;
-};
+interface HomeProps {
+  searchParams: SearchParams;
+}
 
-export default async function Home({ searchParams = {} }: HomeProps) {
+export default async function Home({ searchParams }: HomeProps) {
   return (
     <div className="p-8">
       <Container>
