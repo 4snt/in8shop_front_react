4f68799b7bd9b5d980712af5f6407ce9db9bb866
4snt <murilo.escobedo@protonmail.com>
2025-06-15 20:12:34 -0300

    (fix)?: vou ficar lele

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,7 @@ interface SearchParams {
   [key: string]: string | string[] | undefined;
 }
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: SearchParams;
-}) {
+export default function Home({ searchParams }: { searchParams: SearchParams }) {
   return (
     <div className="p-8">
       <Container>
